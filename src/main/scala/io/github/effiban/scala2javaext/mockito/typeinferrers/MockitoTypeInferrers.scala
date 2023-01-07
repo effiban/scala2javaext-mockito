e4504f5440c7ccde678d1816d581a4466a8929d2
Effi Ban <effi.ban@kenshoo.com>
2023-01-07 20:26:23 +0200

    Add `MockitoApplyTypeInferrer`

--- src/main/scala/io/github/effiban/scala2javaext/mockito/typeinferrers/MockitoTypeInferrers.scala
+++ src/main/scala/io/github/effiban/scala2javaext/mockito/typeinferrers/MockitoTypeInferrers.scala
@@ -1,8 +1,10 @@
 package io.github.effiban.scala2javaext.mockito.typeinferrers
 
-import io.github.effiban.scala2java.spi.typeinferrers.{ApplyTypeTypeInferrer, ExtendedTypeInferrers}
+import io.github.effiban.scala2java.spi.typeinferrers.{ApplyTypeInferrer, ApplyTypeTypeInferrer, ExtendedTypeInferrers}
 
 trait MockitoTypeInferrers extends ExtendedTypeInferrers {
+
+  override def applyTypeInferrer(): ApplyTypeInferrer = MockitoApplyTypeInferrer
 
   override def applyTypeTypeInferrer(): ApplyTypeTypeInferrer = MockitoApplyTypeTypeInferrer
 }
