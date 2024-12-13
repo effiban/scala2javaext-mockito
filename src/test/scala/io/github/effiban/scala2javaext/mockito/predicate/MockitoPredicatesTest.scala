bd8aae2540b25c4104bf174b431207dfbc75b545
effiban <effi.ban@skai.io>
2024-12-13 14:45:35 +0200

    Upgrade scala2java-spi to 10.2.0

--- src/test/scala/io/github/effiban/scala2javaext/mockito/predicate/MockitoPredicatesTest.scala
+++ src/test/scala/io/github/effiban/scala2javaext/mockito/predicate/MockitoPredicatesTest.scala
@@ -5,9 +5,4 @@ import io.github.effiban.scala2javaext.mockito.testsuites.UnitTestSuite
 class MockitoPredicatesTest extends UnitTestSuite {
 
   private val mockitoPredicates = new MockitoPredicates {}
-  import mockitoPredicates._
-
-  test("templateInitExcludedPredicate() should return MockitoTemplateInitExcludedPredicate") {
-    templateInitExcludedPredicate() shouldBe MockitoTemplateInitExcludedPredicate
-  }
 }
